745bdfb1729062e8527183ba4437adf474bf7ae8
Paddywan <paddy@bitnibbler.co.uk>
2019-10-04 08:12:16 +0100

    Implemented project Template

---
+++ BepinTemplate/BepinTemplate.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using RoR2.Networking;
+using UnityEngine.Networking;
+using R2API.Utils;
+using MonoMod.Cil;
+using System.Reflection;
+using BepInEx.Configuration;
+
+namespace Paddywan
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Paddywan." + modname, modname, modver)]
+    public class BepinTemplate : BaseUnityPlugin
+    {
+        private const string modname = "BepinTemplate", modver = "1.0.0";
+    }
+}
