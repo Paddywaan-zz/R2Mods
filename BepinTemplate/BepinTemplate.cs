206bc4005387b9fbfdb4f57a9965f675e4bcd3dc
Paddywan <paddy@bitnibbler.co.uk>
2019-10-05 19:27:32 +0100

    Updated BepinTemplate with .netstandard 2.0 project buildevents

--- BepinTemplate/BepinTemplate.cs
+++ BepinTemplate/BepinTemplate.cs
@@ -8,12 +8,22 @@ using MonoMod.Cil;
 using System.Reflection;
 using BepInEx.Configuration;
 
-namespace Paddywan
+namespace YourUsername
 {
+    /// <summary>
+    /// WARNING! THIS IS THE BASE TEMPLATE, BUILDING THIS PROJECT WILL EXECUTE A UTILITY/HELPER WHICH FINDS YOUR DEFAULT ROR2 INSTALL LOCATION,
+    /// AND WILL PLACE THE BUILT MOD/DLL INTO A /Plugins/dev/ FOLDER! Source: https://github.com/Paddywaan/R2Mods/blob/master/QuerySteamGameLoc
+    /// You can prevent this automated execution by removing the first block inside the "Build-Events" tab of the Project Properties, and replacing it with the hardcoded alternative:
+    /// if $(ConfigurationName) == Debug (copy /Y "$(TargetDir)$(TargetFileName)" "C:\Program Files\SteamLibrary\steamapps\common\Risk of Rain 2\BepInEx\plugins\dev\$(TargetFileName)")
+    /// </summary>
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Paddywan." + modname, modname, modver)]
+    [BepInPlugin("com.YourUsername." + modname, modname, modver)]
     public class BepinTemplate : BaseUnityPlugin
     {
         private const string modname = "BepinTemplate", modver = "1.0.0";
+        public void Awake()
+        {
+
+        }
     }
 }
